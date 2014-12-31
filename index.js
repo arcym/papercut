7f6e07a75d6ead000f4a2df7555ad88c2e556510
Andrew McPherson <andrewmcp333@gmail.com>
2014-12-31 14:35:51 -0500

    Imported the styles and scripts for a gameframe.

---
+++ index.js
@@ -0,0 +1,5 @@
+window.React = require("react/addons")
+window.Reflux = require("reflux")
+
+var GameFrame = require("<root>/scripts/components/GameFrame")
+React.render(<GameFrame/>, document.body)
---
+++ scripts/components/GameFrame.js
@@ -0,0 +1,11 @@
+var GameFrame = React.createClass({
+    render: function() {
+        return (
+            <div id="game-frame">
+                Hello World!
+            </div>
+        )
+    }
+})
+
+module.exports = GameFrame
