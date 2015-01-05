1e6c148cd8fea0fe8209029283d7a49b402b2564
Andrew McPherson <andrewmcp333@gmail.com>
2015-01-05 00:28:37 -0500

    Researched into automatically deploying to github.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require("gulp");
 var gulp_sass = require("gulp-sass");
 var gulp_autoprefixer = require("gulp-autoprefixer");
+var gulp_git = require("gulp-git");
 
 var stream = require("vinyl-source-stream");
 
@@ -8,34 +9,53 @@ var browserify = require("browserify");
 var reactify = require("reactify");
 var aliasify = require("aliasify");
 
-gulp.task("markup", function() {
+gulp.task("markup", function()
+{
     return gulp.src("./source/index.html")
                .pipe(gulp.dest("./build/"))
 });
 
-gulp.task("styles", function() {
+gulp.task("styles", function()
+{
     return gulp.src("./source/index.scss")
                .pipe(gulp_sass())
                .pipe(gulp_autoprefixer())
                .pipe(gulp.dest("./build/"))
 });
 
-gulp.task("scripts", function() {
+gulp.task("scripts", function()
+{
     return browserify("./source/index.js")
                .transform("reactify")
                .transform("aliasify")
-               //.transform("envify")
-               //.transform("uglifyify")
                .bundle()
                .pipe(stream("index.js"))
                .pipe(gulp.dest("./build/"))
 });
 
-gulp.task("assets", function() {
-    return gulp.src("./source/assets/**/*")
+gulp.task("assets", function()
+{
+    return gulp.src("./source/assets/**/*.*")
                .pipe(gulp.dest("./build/assets/"))
 });
 
-gulp.task("default", function() {
-    gulp.start("markup", "styles", "scripts", "assets");
+gulp.task("default", function()
+{
+    return gulp.start(["markup", "styles", "scripts", "assets"]);
+});
+
+gulp.task("deploy", function()
+{
+    return gulp_git.checkout("gh-pages", function(error)
+    {
+        if(error)
+        {
+            throw error;
+        }
+        else
+        {
+            return gulp.src("./source/index.html")
+                       .pipe(gulp_git.add())
+        }
+    });
 });
