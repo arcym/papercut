2c7d25d710c52c26069a210b532f3bc1f56c0fb9
arcym <andrewmcp333@gmail.com>
2015-01-06 17:34:38 -0500

    Fixed the deployment with force.

--- deploy.bat
+++ deploy.bat
@@ -1,8 +1,8 @@
 cd builds
 git init
-git remote add origin master
+git remote add origin https://github.com/arcym/papercut.git
 git checkout -b gh-pages
 git add --all
 git commit -m "Deploying to Github."
-git push origin gh-pages
+git push origin gh-pages -f
 cd ..
