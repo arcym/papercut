d0cc8098b2ebebb15ef0a3cf1ffd383a13e4bc8e
Andrew McPherson <andrewmcp333@gmail.com>
2015-01-08 08:33:30 -0500

    Updated the buildpipe for trimming the package.

--- buildpipe.js
+++ buildpipe.js
@@ -1,6 +1,7 @@
 var gulp = require("gulp")
 var gulp_sass = require("gulp-sass")
 var gulp_autoprefixer = require("gulp-autoprefixer")
+var gulp_json = require("gulp-json-transform")
 
 var vinyl_buffer = require("vinyl-buffer")
 var vinyl_source = require("vinyl-source-stream")
@@ -39,4 +40,11 @@ module.exports.assets = function()
 module.exports.configs = function(dir)
 {
     return gulp.src("./package.json")
+               .pipe(gulp_json(function(data)
+               {
+                   delete data["aliasify"]
+                   delete data["dependencies"]
+                   delete data["devDependencies"]
+                   return data
+               }, 2))
 }
